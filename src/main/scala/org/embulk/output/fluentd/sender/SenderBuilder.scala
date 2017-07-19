ac44824d6973bf89281e27627e735ccd7ea6f0c0
smdmts <smdmts@gmail.com>
2017-07-19 21:06:47 +0900

    Merge remote-tracking branch 'origin/master' into feat_map_reduce_executor_support
    Conflicts:
    	src/main/scala/org/embulk/output/fluentd/sender/SenderBuilder.scala

--- src/main/scala/org/embulk/output/fluentd/sender/SenderBuilder.scala
+++ src/main/scala/org/embulk/output/fluentd/sender/SenderBuilder.scala
@@ -3,7 +3,6 @@ package org.embulk.output.fluentd.sender
 import java.time.Instant
 
 import akka.actor.ActorSystem
-import com.typesafe.config.ConfigFactory
 import org.embulk.output.fluentd.PluginTask
 import org.embulk.spi.Exec
 import wvlet.airframe.{Design, newDesign}
@@ -14,15 +13,15 @@ object SenderBuilder {
   def apply(task: PluginTask): Design = {
     implicit val logger = Exec.getLogger(classOf[Sender])
     implicit val system = ActorSystem("fluentd-sender",
-      ConfigFactory.load(this.getClass.getClassLoader, "application.conf"),
-      this.getClass.getClassLoader)
-
+                                      ConfigFactory.load(this.getClass.getClassLoader, "application.conf"),
+                                      this.getClass.getClassLoader)
     val timeKeyOpt = if (task.getTimeKey.isPresent) {
       Some(task.getTimeKey.get())
     } else None
+
     newDesign
       .bind[SenderFlow]
-      .toInstance(SenderFlowImpl(task.getTag, Instant.now().getEpochSecond, Option(task.getTimeKey)))
+      .toInstance(SenderFlowImpl(task.getTag, Instant.now().getEpochSecond, timeKeyOpt))
       .bind[ActorManager]
       .toInstance(ActorManagerImpl())
       .bind[Sender]
